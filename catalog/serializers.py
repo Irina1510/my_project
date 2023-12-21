d3ddcd4b67438eb573cf5ce5a2c619d0d180f5c3
ПОЛЬЗОВАТЕЛЬ <sttepa@mail.ru>
2023-12-22 01:05:19 +0300

    feat: CRUD Room, Booking, Review

--- catalog/serializers.py
+++ catalog/serializers.py
@@ -1,10 +1,28 @@
 from rest_framework import serializers
-from .models import Hotel
+from .models import Hotel, Room, User, Booking, Review
 
 class HotelSerializer(serializers.ModelSerializer):
      class Meta:
         model = Hotel
         fields = ('id', 'name', 'location', 'description', 'photos', 'rating')
 
+class RoomSerializerUser(serializers.ModelSerializer):
+    hotel_id = HotelSerializer()
+    class Meta:
+        model = Room
+        fields = ('id', 'hotel_id', 'room_type', 'photos', 'price_per_night', 'available')
 
+class RoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = ('id', 'hotel_id', 'room_type', 'photos', 'price_per_night', 'available')
 
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = ('id', 'user_id', 'room_id', 'check_in_date', 'check_out_date')
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = ('id', 'user_id', 'hotel_id', 'comment', 'rating', 'created_at', 'updated_at')
--- catalog/urls.py
+++ catalog/urls.py
@@ -5,4 +5,11 @@ urlpatterns = [
     path('hotellist/', HotelAPIList.as_view()),
     path('hotellist/<int:pk>/', HotelAPIUpdate.as_view()),
     path('hoteldetail/<int:pk>/', HotelAPIDetail.as_view()),
+    path('roomlist/', RoomAPIList.as_view()),
+    path('roomlist/<int:pk>/', RoomAPIUpdate.as_view()),
+    path('roomdetail/<int:pk>/', RoomAPIDetail.as_view()),
+    path('bookinglist/', BookingAPIList.as_view()),
+    path('bookingdetail/<int:pk>/', BookingAPIDetail.as_view()),
+    path('reviewlist/', ReviewAPIList.as_view()),
+    path('reviewdetail/<int:pk>/', ReviewAPIDetail.as_view()),
 ]
